a34fc6d2f64463441197f87db66c793dcefa1752
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-14 19:00:06 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,10 +7,10 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  new_array = []
+  new_array = [] # Not needed :)
 
   array.sort do |x,y|
-    x.length <=> y.length # Takes one item from the array. and compares it against the length of the second
+    x.length <=> y.length # Takes one item from the array. and compares it against the length of the second. The original array is unchanged
   end
 
   # arr.sort{|x, y| x.length <=> y.length}
